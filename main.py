f0c9427a13427fb695c70e1f7775f79543e9cc4c
naufalfm <naufalfm0108@gmai.com>
2025-01-16 17:53:05 +0700

    update main

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import os
 import numpy as np, pandas as pd
-from pyspark.sql import SparkSession
+from pyspark import pandas as ps
 from datasets import load_dataset
 
 
-spark = SparkSession.builder.getOrCreate()
-
 if __name__ == "__main__":
-    df = pd.read_csv(r"C:\Users\naufal\Startup\datasets\corporate_stress_dataset.csv", sep=",")
-    df.head()
+    pass
